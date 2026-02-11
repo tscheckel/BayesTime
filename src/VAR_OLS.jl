7f10d9cd577fc3a1865d9c48dba0c5f9e76706fc
tscheckel <tobias.scheckel@hotmail.de>
2026-02-11 14:03:33 +0100

    now preserving input names for better interpretability of results

--- src/VAR_OLS.jl
+++ src/VAR_OLS.jl
@@ -21,32 +21,57 @@ Estimate a Vector Autoregression via Ordinary Least Squares.
 Tobias Scheckel
 """
 function VAR_OLS(;
-    data::Union{Matrix{Float64}, DataFrame}, Exo::Union{Matrix{Float64}, DataFrame, nothing} = nothing,
+    data::Union{AbstractMatrix{<:Real}, DataFrame}, Exo::Union{AbstractMatrix{<:Real}, DataFrame, Nothing} = nothing,
     P::Int = 1, cons::Bool = true,
     quant::Vector{Float64} = [.025; .5; .975]
 )
     # CHECK INPUT DATATYPES
     if isa(data, DataFrame)
-        data = Matrix{Float64}(data)
+        data = NamedArray(
+            Matrix{Float64}(data),
+            (collect(axes(data, 1)), collect(names(data))),
+            ("obs", "vars")
+        )
     end 
     if isa(Exo, DataFrame)
-        Exo = Matrix{Float64}(Exo)
+        Exo = NamedArray(
+            Matrix{Float64}(Exo),
+            (collect(axes(Exo, 1)), collect(names(Exo))),
+            ("obs", "exo")
+        )
     end
     # get quantiles of standard Gaussian (for CIs)
     normq = quantile.(Normal(), quant)
 
+    data_var_names = collect(1:size(data, 2))
+    if data isa NamedArray
+        data_var_names = collect(names(data, 2))
+    end
+    
     # ---- DATA ----
     # create matrix of dependent variables and regressors
-    Y = data[P+1:end,:]
+    Y = Matrix{Float64}(data[P+1:end,:])
     T, M = size(Y)
     # create design matrix
-    X = mlag(data, P)
+    X_lag = mlag(data, P)
+    X = Matrix{Float64}(X_lag)
+    reg_names = collect(1:size(X, 2))
+    if X_lag isa NamedArray
+        reg_names = collect(names(X_lag, 2))
+    end
     # add exogenous regressors if provided
     if !isnothing(Exo)
-        X = hcat(X, Exo[P+1:end,:])
+        Exo_slice = Exo[P+1:end,:]
+        X = hcat(X, Matrix{Float64}(Exo_slice))
+        exo_names = ["exo_$i" for i in 1:size(Exo_slice, 2)]
+        if Exo_slice isa NamedArray
+            exo_names = collect(names(Exo_slice, 2))
+        end
+        reg_names = vcat(reg_names, exo_names)
     end
     if cons 
         X = hcat(X, fill(1.0, T))
+        reg_names = vcat(reg_names, ["const"])
     end
     K = size(X, 2)
     
@@ -69,6 +94,17 @@ function VAR_OLS(;
     for qx in 1:length(quant)
         Alpha[:,:,qx] = Alpha_hat + reshape(diag(Alpha_V), K, M)*normq[qx]
     end
+
+    Alpha = NamedArray(
+        Alpha,
+        (reg_names, data_var_names, quant),
+        ("regressors", "vars", "quantiles")
+    )
+    Sigma_hat = NamedArray(
+        Sigma_hat,
+        (data_var_names, data_var_names),
+        ("vars", "vars")
+    )
 
     retDict = Dict(
         :Alpha => Alpha,
